6f9eb2bbe921f99294c75db5a712a72788eb0e45
Nabih <nabih@Nabihs-MacBook-Pro.local>
2025-04-07 20:35:51 -0500

    ./tarot.sh

---
+++ tarot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define a list of Tarot cards
+cards=("The Fool" "The Magician" "The High Priestess" "The Empress" "The Emperor" "The Hierophant" "The Lovers" "The Chariot" "Strength" "The Hermit" "Wheel of Fortune" "Justice" "The Hanged Man" "Death" "Temperance" "The Devil" "The Tower" "The Star" "The Moon" "The Sun" "Judgement" "The World")
+
+# Pick 3 random cards
+card1=${cards[$RANDOM % ${#cards[@]}]}
+card2=${cards[$RANDOM % ${#cards[@]}]}
+card3=${cards[$RANDOM % ${#cards[@]}]}
+
+# Output to a file
+echo -e "$card1\n$card2\n$card3" > tarot_reading.txt
+
